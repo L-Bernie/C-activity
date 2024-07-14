f0e7b2e32a6bea114765d1b651e5d5b49a37b03f
L-Bernie <jralonzo1234@gmail.com>
2024-07-14 11:08:04 +0800

    july14

---
+++ other_simple_program.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main(void){
+     //Output of data type by size
+     /*
+     std::cout << " The sizeof char is: " << sizeof(char) << " bytes" << std::endl;
+     std::cout << " The size of float is: " << sizeof(float) << "  bytes" << std::endl;
+     */
+     
+     // sum of two numbers added
+     /*
+     int z,x,y;
+     x = 20;
+     y = 39;
+     z = x+y;
+     std::cout << "sum of two numbers "<< x << " + " << y << " is: " << z << std::endl;
+     */
+     
+     //Swapping two numbers
+     /*
+     int num1, num2, numhold;
+     
+     std::cout << "input first number: " << std::endl;
+     std::cin >> num1;
+     
+     std::cout << "input second number: " << std::endl;
+     std::cin >> num2;
+     
+     numhold = num2;
+     num2 = num1;
+     num1 = numhold;
+     
+     std::cout << "After swapping two numbers: " << std::endl;
+     std::cout << "1st number is: " << num1 << std::endl;
+     std::cout << "2nd number is:" << num2 << std::endl;
+     */
+     
+     //Creating an average operation of 4 numbers
+     /*int f1,f2,f3,f4,average;
+     
+     f1 = 25;
+     f2 = 25;
+     f3 = 25;
+     f4 = 25;
+     
+     average = (f1 + f2 + f3 + f4)/4;
+     
+     std::cout << "the average of 4 numbers is: "<< average << std::endl;
+     */
+}
