f8aa34f99866975f715801e40e74cca880324bb7
L-Bernie <jralonzo1234@gmail.com>
2024-07-14 11:28:05 +0800

    July14

--- other_simple_program.cpp
+++ other_simple_program.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <ctime>
 using namespace std;
 int main(void){
      //Output of data type by size
@@ -36,15 +38,39 @@ int main(void){
      */
      
      //Creating an average operation of 4 numbers
-     /*int f1,f2,f3,f4,average;
-     
+     /*
+     int f1,f2,f3,f4,average;
      f1 = 25;
      f2 = 25;
      f3 = 25;
      f4 = 25;
-     
      average = (f1 + f2 + f3 + f4)/4;
-     
      std::cout << "the average of 4 numbers is: "<< average << std::endl;
+     */
+     
+     //Displayng Time
+     /*
+     time_t t = time(NULL);
+     tm* tPtr = localtime(&t);
+    
+     const int PH_TIME_DIFF = 8 * 3600;
+     time_t ph_time = t + PH_TIME_DIFF;
+     tm* ph_tPtr = gmtime(&ph_time);
+     
+     const char* monthNames[] = {"January", "February", "March", "April", "May", "June", 
+                                "July", "August", "September", "October", "November", "December"};
+                                
+     // Displaying the current date in PHT
+     cout << "Current Date in Philippines: " 
+     << monthNames[ph_tPtr->tm_mon] << "/" 
+     << ph_tPtr->tm_mday << "/" 
+     << (ph_tPtr->tm_year + 1900) << endl; 
+     
+      // Displaying the current time
+     std::cout << "Current Time in PH: "
+     << ph_tPtr->tm_hour << ":"
+     << ph_tPtr->tm_min << ":"
+     << ph_tPtr->tm_sec
+     << std::endl;
      */
 }
